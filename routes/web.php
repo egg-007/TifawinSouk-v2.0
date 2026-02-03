3cff7ec9b9bf18a0154a2320e5f5298da4586c9e
boutaina jad <boutaina.jad.2020@gmail.com>
2026-02-03 16:56:35 +0100

    feat : add categorieRoutes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorieController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('categories', CategorieController::class);
